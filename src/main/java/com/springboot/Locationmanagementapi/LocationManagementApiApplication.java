05a5c8786566250cce707bff12def8a7b1502858
RitikTyagi <ritik.tyagi@unthinkable.co>
2023-01-02 11:55:15 +0530

    project-setup

---
+++ src/main/java/com/springboot/Locationmanagementapi/LocationManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.Locationmanagementapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocationManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocationManagementApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/Locationmanagementapi/LocationManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.Locationmanagementapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocationManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
